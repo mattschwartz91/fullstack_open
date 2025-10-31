4026a9dc830d35a82cd2be8f331e0021f564c806
Matt Schwartz <matschwartz6.0@gmail.com>
2025-10-31 01:16:09 -0400

    finished 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,11 +7,49 @@ const Button = (props) => {
   )
 }
 
+const StatisticLine = ({text, value}) =>{
+  return(
+      <tr>
+        <td>{text} {value}</td>
+      </tr>
+  )
+
+}
+
+const Statistics = (props) => {
+  const {good, bad, neutral} = props
+  const average = (good - bad) / (good + bad + neutral)
+  const positiveReviews = (good) / (good + bad + neutral)
+  const all = good + bad + neutral
+
+  if(all === 0) {
+    return(
+      <p>No feedback given</p>
+    )
+  }
+  return (
+    <>
+      <table>
+        <tbody>
+          <StatisticLine text={"good"} value={good}/>
+          <StatisticLine text={"neutral"} value={neutral}/>
+          <StatisticLine text={"bad"} value={bad}/>
+          <StatisticLine text={"all"} value={all}/>
+          <StatisticLine text={"average"} value={average}/>
+          <StatisticLine text={"positive"} value={positiveReviews}/>
+        </tbody>
+      </table>
+
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const stats = {good: good, neutral: neutral, bad:bad}
 
   const goodReview = () => {
     setGood(good + 1)
@@ -31,9 +69,8 @@ const App = () => {
       <Button onClick={goodReview} text={"good"}/>
       <Button onClick={neutralReview} text={"neutral"}/>
       <Button onClick={badReview} text={"bad"}/>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <h1>statistics</h1>
+      <Statistics good={stats.good} bad={stats.bad} neutral={stats.neutral}/>
     </div>
   )
 }
